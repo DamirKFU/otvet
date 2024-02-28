9f008f55b0caab01bd447af889670de32925d0c0
Shalltear-ov <159260184+DamirKFU@users.noreply.github.com>
2024-02-28 16:52:37 +0300

    add

---
+++ lyceum/homepage/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, Client
+
+
+class HomePageEndPointTest(TestCase):
+    def test_homepage_main_endpoint(self):
+        respone = Client().get("")
+        status_code = respone.status_code
+        self.assertEqual(status_code, 200)
+
+    def test_content_main_homepage(self):
+        respone = Client().get("")
+        content = respone.content.decode()
+        self.assertEqual(content, "Домашняя страница")
