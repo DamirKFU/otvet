cc9818593ee890ae1b03c493945298c32bc14e20
Shalltear-ov <159260184+DamirKFU@users.noreply.github.com>
2024-02-28 16:56:53 +0300

    d

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -5,7 +5,7 @@ class HomePageEndPointTest(TestCase):
     def test_homepage_main_endpoint(self):
         respone = Client().get("")
         status_code = respone.status_code
-        self.assertEqual(status_code, 201)
+        self.assertEqual(status_code, 200)
 
     def test_content_main_homepage(self):
         respone = Client().get("")
